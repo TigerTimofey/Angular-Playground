d30ef855c734eb68b4e50278fcc2750289fdd659
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2025-03-11 20:08:06 +0200

    enhance user component to emit selection event; update app component to handle user selection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,11 +8,15 @@ import { Component, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get ImagePath() {
     return 'assets/users/' + this.avatar;
   }
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
